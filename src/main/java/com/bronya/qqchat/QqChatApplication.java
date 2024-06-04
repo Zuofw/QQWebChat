eb4bdcb4302685cb8f6941d7cd665c3ca6be4ccd
zuowei <2354796263@qq.com>
2024-06-04 09:02:13 +0800

    feat:初始化

---
+++ src/main/java/com/bronya/qqchat/QqChatApplication.java
@@ -0,0 +1,13 @@
+package com.bronya.qqchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QqChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bronya/qqchat/QqChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bronya.qqchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QqChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
